efb8bcfbcb78bf26dab9472ec0b8c3423ca41fa1
Marlon Trujillo Ariza <marlon.trujillo2001@gmail.com>
2020-08-12 17:20:53 -0700

    Initial Commit

---
+++ Set Card Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set Card Game
+//
+//  Created by Marlon Trujillo Ariza on 8/12/20.
+//  Copyright © 2020 Marlon Trujillo Ariza. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
